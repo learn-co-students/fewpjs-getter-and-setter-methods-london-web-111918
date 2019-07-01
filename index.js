e27e2603507c150d3f3eff878340c04542c4354b
Joaquim Barreto <joaquim@barreto.co>
2019-07-01 11:51:35 +0100

    diamter

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 class Circle {
-    constructor(radius) {
-        this.radius = radius
-    }
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  get diameter() {
+    return this.radius * 2;
+  }
+
+  set diameter(diameter) {
+    this.radius = diameter / 2;
+  }
 }
